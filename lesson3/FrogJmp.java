98a492e8cf28d94b04cfb6dd029a7637788d3885
Nikolay Nikolaev <evrinoma@gmail.com>
2017-12-23 23:42:13 +0300

    FrogJmp lesson3 v1 fix

---
+++ lesson3/FrogJmp.java
@@ -0,0 +1,32 @@
+//*******************************************************************
+import java.util.*;
+
+public class HelloWorld
+{
+  
+  public static void main(String[] args)
+  {
+    Solution myObject = new Solution();
+    myObject.print(myObject.solution(10,85,30));
+    myObject.print(myObject.solution(1,1,3));
+    myObject.print(myObject.solution(3,999111321,7));
+    myObject.print(myObject.solution(10,5,2));
+  }
+}
+
+
+class Solution {
+    
+    public void print(int A)
+    {
+    	System.out.println('['+A+']');
+    }
+  
+
+    public int[] solution(int[] A, int K) {
+	int length = (Y - X);
+	return (X != Y) ? (length / D) + (0 == length % D ? 0 : 1) : 0;
+    }
+}
+  
+  
